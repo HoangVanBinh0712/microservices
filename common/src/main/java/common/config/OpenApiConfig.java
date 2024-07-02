f9eb77007fd5d210976be50f3752ac95b369b01b
LAPTOP-GK940M8V\ASUS <thebest11447@gmail.com>
2024-07-02 22:24:01 +0700

    Config Docker, Kubernetes => deploy aws eks

--- common/src/main/java/common/config/OpenApiConfig.java
+++ common/src/main/java/common/config/OpenApiConfig.java
@@ -5,10 +5,13 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 @ComponentScan
@@ -17,13 +20,25 @@ public class OpenApiConfig {
     @Value("${name}")
     private String appName;
 
+    @Value("${swagger.gateway-url}")
+    private String gatewayUrl;
+
+    @Value("${swagger.description}")
+    private String description;
+
     @Bean
     public OpenAPI customizeOpenAPI() {
         final String securitySchemeName = "bearerAuth";
         Info info = new Info();
         info.setTitle(appName);
+
+        Server sv = new Server();
+        sv.url(gatewayUrl);
+        sv.description(description);
+
         return new OpenAPI()
                 .info(info)
+                .servers(List.of(sv))
                 .addSecurityItem(new SecurityRequirement()
                         .addList(securitySchemeName))
                 .components(new Components()
